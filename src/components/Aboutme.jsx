70b2573a7fbefd9171b2c15bfd6286c858c617b5
yashwant <gurjaryash00@gmail.com>
2024-04-28 15:52:36 +0530

    tranfering_code_into_components

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import photo1 from "../../assets/photo1.jpg";
+
+
+function Aboutme() {
+  return (
+    <div>
+      <div className="fourth-component p-8 ">
+        <h3 className="text-xl font-semibold tracking-wider">About Me</h3>
+        <div className="fourth-component-text border-2 border-gray-800 mt-5">
+          <h2 className="text-3xl font-bold p-8">Get to know me better.</h2>
+          <div className="main-bottom-area flex h-32 border-t-2 border-b-2 border-gray-800 ">
+            <div className="inner-text flex items-center p-8  border-gray-800 ">
+              <img
+                className="rounded-full"
+                src={photo1}
+                alt="img"
+                width="100px"
+                hight="100px"
+              />
+              <h3 className="text-3xl font-bold p-3">Yashwant Gurjar</h3>
+            </div>
+            <div className="border-l-2 border-gray-800 text-xl font-semibold  p-12">
+              Liked by you and 6 others
+            </div>
+          </div>
+          <div className="main-end-area">
+            <p className="text-lg font-medium p-10 break-all">
+              Passionate about the realm of technology, I am a results-driven
+              Front-end software developer with a solid background in frontend
+              development. Proficient in languages such as JavaScript and
+              currently expanding my skills with TypeScript, I also possess
+              framework knowledge in React.js and Tailwind.css. My focus lies in
+              crafting robust and scalable applications that tackle complex
+              issues while delivering exceptional user experiences. I am deeply
+              passionate about developing new products with a strong focus on
+              utilizing effective technology. I am proficient in conducting
+              thorough research and possess some knowledge of marketing
+              strategies. I find great satisfaction in conceptualizing and
+              creating products, and I am committed to ensuring their success.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Aboutme
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import html from "../../assets/html.png";
+import css from "../../assets/css.jpg";
+import js from "../../assets/js.png";
+import typescript from "../../assets/typescript.png";
+import tailwind from "../../assets/tailwind.png";
+import redux1 from "../../assets/redux1.jpg";
+import github1 from "../../assets/github1.png";
+import react_icon2 from "../../assets/react-icon-2.png";
+
+// import {prettier} from "../../assets/prettier2.png";
+
+function Technologies() {
+  return (
+    <div>
+      <div className="fifth-component p-8">
+        <h3 className="text-xl font-semibold tracking-wider">TECNOLOGIES</h3>
+        <div className="fifth-component-main border-2 border-gray-800  mt-5">
+          <h2 className="fifth-component-text border-b-2 border-gray-800 text-3xl font-bold p-8">
+            Check out what I'm good at.
+          </h2>
+          <div className="fifth-component-images m-5">
+            <div className="first_components_tech flex justify-around items-center">
+              <div className="flex flex-col items-center">
+                <img src={html} alt="_" width="100px" hight="100px" />
+                <p className="text-lg font-bold">HTML</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={css} alt="_" width="100px" hight="100px" />
+                <p className="text-lg font-bold">CSS</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={js} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold">JS</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={typescript} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold">TS</p>
+              </div>
+            </div>
+            <div className="second_components_tech flex justify-around items-center pt-8">
+              <div className="flex flex-col items-center">
+                <img src={react_icon2} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold">React</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={tailwind} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold pt-8">Tailwind</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={redux1} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold">Redux</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={github1} alt="" width="100px" hight="100px" />
+                <p className="text-lg font-bold">GitHub</p>
+              </div>
+            </div>
+            {/* <div >
+
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Technologies
