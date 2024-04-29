d2d010bfbdb8a1e8af8ba57096d24e6be1560373
yashwant <gurjaryash00@gmail.com>
2024-04-29 15:20:48 +0530

    adding_contact_Form

--- src/App.jsx
+++ src/App.jsx
@@ -5,99 +5,16 @@ import Firstsection from "./components/Firstsection";
 import Aboutme from "./components/Aboutme";
 import Technologies from "./components/Technologies";
 import Project from "./components/Project";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="main">
-     <Firstsection/>
-      <Aboutme/>
-     <Technologies/>
-    <Project/>
-      
-      {/* <div className="sixth_component_project p-8">
-        <h3 className="text-xl font-semibold tracking-wider ">PROJECTS</h3>
-        <div className="sixth_component_project_main border-2 border-gray-800 mt-5 ">
-          <h2 className="sixth_component_project_text border-b-2 border-gray-800 text-3xl font-bold p-8">
-            Take a look what I've been working on.
-          </h2>
-          <div className="sixth_component_project_images">
-            <div className="image ">
-              <img src={prettier} alt="" width={"auto"} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="seventh_components first_ptoject p-8">
-        <h3 className="text-xl font-semibold tracking-wider">01.</h3>
-        <div className="senvth_component  border-2 border-gray-800 mt-5 ">
-          <div className="senvth_component_heading border-b-2 border-gray-800 flex  items-center justify-between p-8">
-            <h2 className="senvth_component_text    text-3xl font-bold ">
-              Calacs
-            </h2>
-            <div className="buttons flex items-center">
-              <Button type="link" className="text-black">
-                code
-              </Button>
-              <Flex>
-                <Button type="primary" className="bg-black">
-                  Visit
-                </Button>
-              </Flex>
-            </div>
-          </div>
-          <div className="senvth_component_images">
-            <img src={project1} alt="_" width={"75%"} className="m-auto" />
-          </div>
-        </div>
-      </div>
-      <div className="seventh_components first_ptoject p-8">
-        <h3 className="text-xl font-semibold tracking-wider ">02.</h3>
-        <div className="senvth_component  border-2 border-gray-800 mt-5">
-          <div className="senvth_component_heading border-b-2 border-gray-800 flex  items-center justify-between p-8">
-            <h2 className="senvth_component_text    text-3xl font-bold ">
-              Response.AI
-            </h2>
-            <div className="buttons flex items-center">
-              <Button type="link" className="text-black">
-                code
-              </Button>
-              <Flex>
-                <Button type="primary" className="bg-black">
-                  Visit
-                </Button>
-              </Flex>
-            </div>
-          </div>
-          <div className="senvth_component_images">
-            <img src={project2} alt="_" width={"75%"} className="m-auto" />
-          </div>
-         
-        </div>
-      </div>
-      <div className="seventh_components first_ptoject p-8">
-        <h3 className="text-xl font-semibold tracking-wider">03.</h3>
-        <div className="senvth_component  border-2 border-gray-800 mt-5">
-          <div className="senvth_component_heading border-b-2 border-gray-800 flex  items-center justify-between p-8">
-            <h2 className="senvth_component_text    text-3xl font-bold ">
-              Personal Portfolio
-            </h2>
-            <div className="buttons flex items-center ">
-              <Button type="link" className="text-black ">
-                code
-              </Button>
-              <Flex>
-                <Button type="primary" className="bg-black ">
-                  Visit
-                </Button>
-              </Flex>
-            </div>
-          </div>
-          <div className="senvth_component_images">
-            <img src={project3} alt="_" width={"75%"} className="m-auto" />
-          </div>
-          
-        </div>
-      </div> */}
+      <Firstsection />
+      <Aboutme />
+      <Technologies />
+      <Project />
+      <Contact/>
     </div>
   );
 }
